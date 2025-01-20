44db914d09687758f1e2967a0f040ac020b6649a
Sebastian Richter <sebastian.richter@paessler.com>
2025-01-20 16:28:27 +0100

    -introduce memory.

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,40 @@ const ray = @cImport({
     @cInclude("raylib.h");
 });
 
+const Memory = struct {
+    memory: [0xFFFF]u8,
+
+    const ROM_START = 0x8000;
+    const ROM_END = 0xFFFF;
+    const ROM_PROGRAM_START = 0xFFFC;
+
+    pub fn init(program: []const u8) Memory {
+        const program_end = ROM_START + program.len;
+
+        if (program_end > ROM_END) {
+            std.debug.panic("Program size exceeds available memory space!\n", .{});
+        }
+
+        var memory = Memory{ .memory = [_]u8{0} ** 0xFFFF };
+        @memcpy(memory.memory[ROM_START..program_end], program);
+        memory.write_u16(Memory.ROM_PROGRAM_START, Memory.ROM_START);
+
+        return memory;
+    }
+
+    pub fn read_u16(self: *Memory, pos: u16) u16 {
+        var buf = [_]u8{ self.memory[pos], self.memory[pos + 1] };
+        return std.mem.readInt(u16, &buf, .little);
+    }
+
+    pub fn write_u16(self: *Memory, pos: u16, data: u16) void {
+        var buf: [2]u8 = undefined;
+        std.mem.writeInt(u16, &buf, data, .little);
+        self.memory[pos] = buf[0];
+        self.memory[pos + 1] = buf[1];
+    }
+};
+
 const Cpu = struct {
     program_counter: u16,
     status: u8,
@@ -18,6 +52,13 @@ const Cpu = struct {
         };
     }
 
+    pub fn reset(self: *Cpu, mem: *Memory) void {
+        self.program_counter = mem.read_u16(Memory.ROM_PROGRAM_START);
+        self.status = 0;
+        self.register_A = 0;
+        self.register_X = 0;
+    }
+
     const ZERO_FLAG: u8 = 0b0000_0010;
     const NEGATIVE_FLAG: u8 = 0b1000_0000;
 
@@ -26,14 +67,9 @@ const Cpu = struct {
     const LDA = 0xA9;
     const TAX = 0xAA;
 
-    pub fn interpret(self: *Cpu, program: []const u8) void {
-        if (program.len > std.math.maxInt(u16)) {
-            std.debug.panic("Program extends the allowed size of maximum memeory size, got {}\n", .{program.len});
-        }
-
-        self.program_counter = 0;
+    pub fn interpret(self: *Cpu, memory: Memory) void {
         while (true) {
-            const ops_code = program[self.program_counter];
+            const ops_code = memory.memory[self.program_counter];
             self.program_counter += 1;
 
             switch (ops_code) {
@@ -45,7 +81,7 @@ const Cpu = struct {
                     self.update_zero_and_negative_flag(&self.register_X);
                 },
                 LDA => {
-                    self.register_A = program[self.program_counter];
+                    self.register_A = memory.memory[self.program_counter];
                     self.program_counter += 1;
                     self.update_zero_and_negative_flag(&self.register_A);
                 },
@@ -73,7 +109,19 @@ const Cpu = struct {
     }
 };
 
+pub fn load_and_run_program(cpu: *Cpu, program: []const u8) Memory {
+    var mem = Memory.init(program);
+    cpu.reset(&mem);
+    cpu.interpret(mem);
+
+    return mem;
+}
+
 pub fn main() !void {
+    var mem = Memory.init(&[_]u8{ 0x01, 0x02 });
+    mem.write_u16(0, 0x8000);
+    std.debug.print("foo 0x{X}", .{mem.read_u16(0)});
+
     ray.InitWindow(400, 400, "NES");
     ray.SetTargetFPS(60);
 
@@ -88,7 +136,7 @@ pub fn main() !void {
 test "0xA9 LDA - Load Accumulator" {
     var cpu = Cpu.init();
 
-    cpu.interpret(&[_]u8{ 0xA9, 0x05, 0x00 });
+    _ = load_and_run_program(&cpu, &[_]u8{ 0xA9, 0x05, 0x00 });
 
     try std.testing.expect(cpu.register_A == 0x05);
     try std.testing.expect((cpu.status & 0b0000_0010) == 0);
@@ -98,29 +146,27 @@ test "0xA9 LDA - Load Accumulator" {
 test "0xAA TAX - Transfer Accumulator to X" {
     var cpu = Cpu.init();
 
-    cpu.register_A = 10;
-    cpu.interpret(&[_]u8{ 0xAA, 0x00 });
+    _ = load_and_run_program(&cpu, &[_]u8{ 0xA9, 0x0A, 0xAA, 0x00 });
 
     try std.testing.expect(cpu.register_X == 10);
     try std.testing.expect((cpu.status & 0b0000_0010) == 0);
     try std.testing.expect((cpu.status & 0b1000_0000) == 0);
 }
 
-test "INX - Increment X Register" {
+test "0xE8 INX - Increment X Register" {
     var cpu = Cpu.init();
 
-    cpu.interpret(&[_]u8{ 0xE8, 0x00 });
+    _ = load_and_run_program(&cpu, &[_]u8{ 0xE8, 0x00 });
 
     try std.testing.expect(cpu.register_X == 1);
     try std.testing.expect((cpu.status & 0b0000_0010) == 0);
     try std.testing.expect((cpu.status & 0b1000_0000) == 0);
 }
 
-test "INX overflow" {
+test "0xE8 INX overflow" {
     var cpu = Cpu.init();
 
-    cpu.register_X = 0xFF;
-    cpu.interpret(&[_]u8{ 0xE8, 0xE8, 0x00 });
+    _ = load_and_run_program(&cpu, &[_]u8{ 0xA9, 0xFF, 0xAA, 0xE8, 0xE8, 0x00 });
 
     try std.testing.expect(cpu.register_X == 1);
 }
@@ -128,7 +174,7 @@ test "INX overflow" {
 test "loads 0xC0 into X and increments by 1" {
     var cpu = Cpu.init();
 
-    cpu.interpret(&[_]u8{ 0xA9, 0xC0, 0xAA, 0xE8, 0x00 });
+    _ = load_and_run_program(&cpu, &[_]u8{ 0xA9, 0xC0, 0xAA, 0xE8, 0x00 });
 
     try std.testing.expect(cpu.register_X == 0xC1);
 }
